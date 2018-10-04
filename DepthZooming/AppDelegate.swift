8e4b867b1f43496f76c118ba8c5e9bb165bf37ef
narumij <narumij@me.com>
2018-10-04 11:55:12 +0900

    initial

---
+++ DepthZooming/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DepthZooming
+//
+//  Created by Jun Narumi on 2018/10/04.
+//  Copyright © 2018 Jun Narumi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DepthZooming/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DepthZooming
+//
+//  Created by Jun Narumi on 2018/10/04.
+//  Copyright © 2018 Jun Narumi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
